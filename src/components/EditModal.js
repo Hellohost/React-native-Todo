95ecd3b3a72e3a1031e5248c5ced2d9a689416a0
Denis <you@example.com>
2020-04-27 14:32:54 +0300

    first commit

---
+++ src/components/EditModal.js
@@ -0,0 +1,60 @@
+import React, {useState} from 'react'
+import {View, StyleSheet, TextInput, Button, Modal, Alert} from 'react-native'
+import { THEME } from '../theme'
+import {AppButton} from './ui/AppButton'
+export const EditModal = ({visible, onCancel,value, onSave}) => {
+    const [title, setTitle] =useState(value)
+
+    const saveHandler = () =>{
+        if (title.trim().length < 3) {
+            Alert.alert('Error!', `Minimal size name 3, now is ${title.trim().length} symbol`)
+        } else {
+            onSave(title)
+        }
+    }
+    return (
+        <Modal visible={visible}
+         animationType='slide'
+         transparent={false}> 
+            <View style={styles.wrap}>
+                <TextInput style={styles.input}
+                value={title}
+                onChangeText={setTitle} 
+                placeholder="Enter name of task"
+                autoCapitalize="none"
+                autoCorrect={false}
+                maxLength={64}
+                />
+                <View style={styles.buttons}>
+                <AppButton  onPress={onCancel} color={THEME.DANGER_COLOR}>
+                    Cancel
+                </AppButton>
+                <AppButton  onPress={saveHandler}>
+                    Save
+                </AppButton>
+
+                </View>
+            </View>
+        </Modal>
+    )
+}
+
+const styles = StyleSheet.create({
+    wrap: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: 'center'
+    },
+    input: {
+        padding: 10,
+        borderBottomColor: THEME.MAIN_COLOR,
+        borderBottomWidth: 2,
+        width: '80%'
+    },
+    buttons: {
+        width: '100%',
+        marginTop: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    }
+})
